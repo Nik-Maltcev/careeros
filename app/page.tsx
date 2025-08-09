72ebcd0e7fb55ed09631cae5ad2d7b3e344f8cf3
Nik-Maltcev <35464161+Nik-Maltcev@users.noreply.github.com>
2025-08-09 22:09:55 +0300

    feat: удалена страница контактов, исправлена белая кнопка на главной

--- app/page.tsx
+++ app/page.tsx
@@ -354,9 +354,6 @@ export default function LandingPage() {
               <Link href="/jobs" className="text-white hover:text-blue-300 transition-colors">
                 Найти вакансии
               </Link>
-              <Link href="/contacts" className="text-white hover:text-blue-300 transition-colors">
-                Контакты
-              </Link>
               <Link href="/demo-profile" className="text-yellow-300 hover:text-yellow-200 transition-colors">
                 Демо профиль
               </Link>
@@ -1028,12 +1025,14 @@ export default function LandingPage() {
                     </Button>
                   </a>
                   
-                  <Link href="/contacts">
-                    <Button variant="outline" className="border-white/20 text-white hover:bg-white/10 px-6 py-3">
-                      <Mail className="w-5 h-5 mr-2" />
-                      Все контакты
-                    </Button>
-                  </Link>
+                  <Button 
+                    variant="outline" 
+                    className="border-blue-500/50 text-blue-300 hover:bg-blue-500/20 hover:border-blue-400 px-6 py-3 bg-transparent"
+                    onClick={() => window.open('mailto:support@careeros.ru', '_blank')}
+                  >
+                    <Mail className="w-5 h-5 mr-2" />
+                    Написать на email
+                  </Button>
                 </div>
                 
                 <div className="mt-6 pt-6 border-t border-white/10">
